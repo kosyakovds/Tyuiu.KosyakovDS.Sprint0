985b6baf9f1c782471390c8e8a7f26db8c7c620a
kosyakovds <kosyakov27@mail.ru>
2025-09-13 16:27:07 +0500

    Task4

---
+++ Tyuiu.KosyakovDS.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.KosyakovDS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
---
+++ Tyuiu.KosyakovDS.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KosyakovDS.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+            Console.WriteLine(DataService.Substraction(10, 5));
+            Console.WriteLine(DataService.Multiplication(10, 5));
+            Console.WriteLine(DataService.Division(9, 3));
+        }
+    }
+}
