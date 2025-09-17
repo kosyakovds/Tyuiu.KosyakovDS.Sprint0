5a1084d34884be0ede92477b477ad2cebe7b81b2
kosyakovds <kosyakov27@mail.ru>
2025-09-17 21:04:38 +0500

    Task5

---
+++ Tyuiu.KosyakovDS.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KosyakovDS.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Substraction(15, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(10, 10));
+            Console.WriteLine("A / B = " + DataService.Division(9, 3));
+        }
+    }
+}
