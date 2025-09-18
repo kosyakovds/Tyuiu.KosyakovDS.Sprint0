2f2da817e803203a9c60693fb3cfaa18a318eb80
kosyakovds <kosyakov27@mail.ru>
2025-09-18 23:17:58 +0500

    Task6

---
+++ Tyuiu.KosyakovDS.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.KosyakovDS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KosyakovDS.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numsArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+        }
+    }
+}
